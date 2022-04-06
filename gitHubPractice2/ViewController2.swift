7a442b408039491e490dd676da85eeb4982329ce
Matthew Sanchez <msanchez2368@stu.d214.org>
2022-04-06 12:37:00 -0500

    Table View, Navigation Controller, ViewController2

---
+++ gitHubPractice2/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  gitHubPractice2
+//
+//  Created by Matthew Sanchez on 4/6/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
